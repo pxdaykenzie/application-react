5388a73f745379fdbaa706b3771234fab61cc1ea
Kyle Negley <KPNegley@gmail.com>
2022-08-31 23:12:01 -0400

    Kenzie React Assessment: Displayed usernames dynamically in App.js.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-
   const users = [
     { name: "Randy Cox", id: 1 },
     { name: "Cody Thaller", id: 2 },
-    { name: "Michelle Hoelzer", id: 3 }
+    { name: "Michelle Hoelzer", id: 3 },
   ];
+
+  const userNames = users.map(({ name, id }) => <li key={id}>{name}</li>);
 
   return (
     <div>
       <h1>User names</h1>
-      <ul></ul>
+      <ul>{userNames}</ul>
     </div>
   );
 }
